d36e1748a82ba418b11c882948dd5edeedab6b24
Michael D. Hoyle <hoylemd@gmail.com>
2014-08-26 11:00:33 -0400

    made install script clean up old stuff better

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 mv ~/.vimrc vimrc.old
 mv ~/.bashrc bashrc.old
 mv ~/.gitconfig gitconfig.old
+rm ~/.vimrc
+rm ~/.bashrc
+rm ~/.gitconfig
 ln .vimrc ~/.vimrc
 ln .bashrc ~/.bashrc
 ln .gitconfig ~/.gitconfig
